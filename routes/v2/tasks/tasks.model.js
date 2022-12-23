591344d5ffd46246787373e86cd33bfbdc09e82d
Varinder Singh <varindersingh271990@gmail.com>
2022-12-23 22:38:12 +0530

    added status enum in task schema

--- routes/v2/tasks/tasks.model.js
+++ routes/v2/tasks/tasks.model.js
@@ -1,20 +1,25 @@
 const mongoose = require("mongoose");
+const { DONE, IN_PROGRESS, OPEN } = require("../../../util/constants");
 const Schema = mongoose.Schema;
 
-const taskSchema = new Schema({
-    title:{
-        type: String,
-        required:true
+const taskSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
     },
-    description:{
-        type: String,
-        required:true
+    description: {
+      type: String,
+      required: true,
     },
-    status:{
-        type: String,
-        required:true,
-        default:"OPEN"
-    }
-}, {timestamps: true});
+    status: {
+      type: String,
+      required: true,
+      enum: [DONE, OPEN, IN_PROGRESS],
+      default: OPEN,
+    },
+  },
+  { timestamps: true }
+);
 
-module.exports = mongoose.model("Task",taskSchema);
+module.exports = mongoose.model("Task", taskSchema);
